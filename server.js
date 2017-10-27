0413df90d6cdfc68772c987bf3d4d7baa9fb5246
Matthias Vanooteghem <matthias.vanooteghem@gmail.com>
2017-10-27 20:49:09 +0200

    dist

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const port = process.env.PORT || 8080;
 
 
 // Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/srcdist'));
+app.use(express.static(__dirname + 'dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(port);
 
 // For all GET requests, send back index.html so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 console.log(`Server listening on ${port}`);
