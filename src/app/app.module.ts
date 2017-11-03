66520e9f0e7b58a31b08f0380f13bc792d31f56c
Matthias Vanooteghem <matthias.vanooteghem@gmail.com>
2017-11-03 22:25:59 +0100

    ratings-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+
+import { RouterModule, Routes } from '@angular/router';
+
 import { RaterComponent } from './rater/rater.component';
+import { RatingsListComponent } from './ratings-list/ratings-list.component';
+
+const appRoutes: Routes = [
+  { path: 'rater', component: RaterComponent },
+  { path: 'ratingslist', component: RatingsListComponent }
+  // { path: 'hero/:id',      component: RaterComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RaterComponent
+    RaterComponent,
+    RatingsListComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ src/app/ratings-list/ratings-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ratings-list',
+  templateUrl: './ratings-list.component.html',
+  styleUrls: ['./ratings-list.component.css']
+})
+export class RatingsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
