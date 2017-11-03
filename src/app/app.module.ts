008eb6185a95555df063db7d6d948f8c6acf9d3f
Matthias Vanooteghem <matthias.vanooteghem@gmail.com>
2017-11-03 16:23:59 +0100

    eerste component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RaterComponent } from './rater/rater.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RaterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/rater/rater.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rater2',
+  templateUrl: './rater.component.html',
+  styleUrls: ['./rater.component.css']
+})
+export class RaterComponent implements OnInit {
+  title = 'app';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
