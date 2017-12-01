eef2d9b30e135b9d655de8e154adbc842a6e665e
Matthias Vanooteghem <matthias.vanooteghem@gmail.com>
2017-12-01 09:22:05 +0100

    heroku init

--- app.js
+++ app.js
@@ -1,17 +1,67 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
+require('dotenv').config();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+let mongoose = require('mongoose');
+let passport = require('passport');
+let LocalStrategy = require('passport-local').Strategy;
 
-const app = express();
+
+require('./models/Rating');
+require('./models/Contact');
+require('./models/User');
+
+require('./config/passport');
+
+mongoose.connect(process.env.RECIPE_DATABASE, {  useMongoClient: true });
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(passport.initialize());
+
+app.use('/', index);
+app.use('/api/users', users);
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+app.all('*', (req, res) => {
+  const indexFile = `${path.join(__dirname, 'dist')}/index.html`;
+  res.status(200).sendFile(indexFile);
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-const port = process.env.PORT || '3001';
-app.set('port', port);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-const server = http.createServer(app);
-server.listen(port, () => console.log(`running`));
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
